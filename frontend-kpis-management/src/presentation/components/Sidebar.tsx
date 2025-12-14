26d7312c1637a93209c1d6aaf9c2ab03921285b9
Felix Castro <castrocadenas99@gmail.com>
2025-12-13 23:49:49 -0500

    repush

--- frontend-kpis-management/src/presentation/components/Sidebar.tsx
+++ frontend-kpis-management/src/presentation/components/Sidebar.tsx
@@ -31,7 +31,7 @@ const menuItems = [
     disabled: true,
   },
   {
-    label: 'Eventos',
+    label: 'Administracion',
     to: '/events',
     icon: FaHeart,
     description: 'Proximamente...',
